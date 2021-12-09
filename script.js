45c74a3a40ea1309d883ea13d2d68dea68497e57
Gustavo Rondello <gustavorondello99@outlook.com>
2021-12-09 16:24:59 -0300

    Desafio 7 completo

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-window.onload{
-    
-}
+window.onload = function () {
+  const collorPalette = document.getElementById('color-palette');
+
+  function classSelected(event) {
+    let selected = document.querySelector('.selected');
+    selected.classList.remove('selected');
+    event.target.classList += ' selected';
+  }
+
+  collorPalette.addEventListener('click', classSelected);
+};
