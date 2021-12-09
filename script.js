561eed0aa419e0af842bd6679e4061fae610fc3c
Gustavo Rondello <gustavorondello99@outlook.com>
2021-12-09 19:04:54 -0300

    Desafio 9 completo

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+/*                       Referencias: 
+https://developer.mozilla.org/pt-BR/docs/Web/API/Element/setAttribute
+https://developer.mozilla.org/pt-BR/docs/Web/API/element/getAttribute
+*/
+
 window.onload = function () {
   const collorPalette = document.getElementById('color-palette');
+  const pixel = document.getElementById('pixel-board');
+  const button = document.getElementById('clear-board');
 
   function classSelected(event) {
     let selected = document.querySelector('.selected');
@@ -7,5 +14,26 @@ window.onload = function () {
     event.target.classList += ' selected';
   }
 
+  function printColor(event) {
+    let selected = document.querySelector('.selected');
+    let colorSelected = selected.getAttribute('id');
+    //   let pixelBlock = document.querySelectorAll('.pixel');
+    //   pixelBlock.setAttribute("id", colorSelected)
+    event.target.setAttribute('id', colorSelected);
+  }
+
+  function clearBoard() {
+    let pixelArray = document.getElementsByClassName('pixel');
+    for (let i = 0; i < pixelArray.length; i += 1) {
+      let pixelBoard = pixelArray[i];
+      pixelBoard.removeAttribute('id');
+    }
+  }
+
   collorPalette.addEventListener('click', classSelected);
+  pixel.addEventListener('click', printColor);
+  button.addEventListener('click', clearBoard);
+  let pixelArray = document.getElementsByClassName('pixel');
+  let pixelBoard = pixelArray[10];
+  console.log(pixelBoard);
 };
